55f24ca52c39edec67d46e6173a9efed65264d23
ZholudV <goldexzholud@gmail.com>
2022-06-01 15:05:35 +0500

    dnjhjq комит

---
+++ src/main/java/context/AppConfig.java
@@ -0,0 +1,10 @@
+package context;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ru.geekbrains.context")
+public class AppConfig {
+
+}
